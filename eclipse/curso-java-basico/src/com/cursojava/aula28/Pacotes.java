9147e5aeb41883a31d19c611ff0c4106554472fb
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-09-02 18:50:51 -0300

    Aula 28 - Pacotes e import

---
+++ eclipse/curso-java-basico/src/com/cursojava/aula28/Pacotes.java
@@ -0,0 +1,17 @@
+package com.cursojava.aula28;
+
+import com.cursojava.aula20.labs.Ex05;
+import com.cursojava.aula20.labs.Ex06;
+import com.cursojava.aula27.Carro;
+
+
+public class Pacotes {
+
+	public static void main(String[] args) {
+		Carro carro;
+		com.cursojava.aula26.Carro carro2;
+		
+		Ex06 ex06;
+		Ex05 ex05;
+	}
+}
