53f73bdd20e6369e0cf0d858f3a2add714824b2b
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-09-04 15:04:26 -0300

    Exercícios Aula 35

---
+++ eclipse/curso-java-basico/src/com/cursojava/aula35/labs/Ex01.java
@@ -0,0 +1,13 @@
+package com.cursojava.aula35.labs;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<10; i++) {
+			System.out.print(Calculadora.fibonacci(i) + " ");
+		}
+
+	}
+
+}
---
+++ eclipse/curso-java-basico/src/com/cursojava/aula35/labs/Ex02.java
@@ -0,0 +1,11 @@
+package com.cursojava.aula35.labs;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Calculadora.somatorio(5));
+
+	}
+
+}
