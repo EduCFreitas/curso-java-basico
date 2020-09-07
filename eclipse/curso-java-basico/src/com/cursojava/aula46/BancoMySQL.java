1d2f1a63461524a605d751c0606558cda3a2f8b8
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-09-07 19:13:09 -0300

    Aula 46 - Interface e Herança (Comparação)

---
+++ eclipse/curso-java-basico/src/com/cursojava/aula46/BancoMySQL.java
@@ -0,0 +1,62 @@
+package com.cursojava.aula46;
+
+//Classe só pode extender uma outra classe e implementar quantas interfaces forem necessárias
+//Interface pode extender quantas interfaces forem necessárias
+public class BancoMySQL implements BancoDados{
+
+	@Override
+	public void grant(String access) {
+		
+	}
+
+	@Override
+	public void revoke(String access) {
+		
+	}
+
+	@Override
+	public void select(String query) {
+		
+	}
+
+	@Override
+	public void insert(String query) {
+		
+	}
+
+	@Override
+	public void delete(String query) {
+		
+	}
+
+	@Override
+	public void update(String query) {
+		
+	}
+
+	@Override
+	public void create(String query) {
+		
+	}
+
+	@Override
+	public void alter(String query) {
+		
+	}
+
+	@Override
+	public void drop(String query) {
+		
+	}
+
+	@Override
+	public void abrirConexao() {
+		
+	}
+
+	@Override
+	public void fecharConexao() {
+		
+	}
+
+}
