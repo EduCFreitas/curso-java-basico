27c245e0afe61fc4ceadc31c3842836134e61751
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-09-07 18:29:12 -0300

    Aula 44 - Interfaces

---
+++ eclipse/curso-java-basico/src/com/cursojava/aula44/AnimalDomesticado.java
@@ -0,0 +1,9 @@
+package com.cursojava.aula44;
+
+public interface AnimalDomesticado {
+
+	public final int ANO = 2020;
+	
+	void levarVeterinario();
+	void alimentar();
+}
---
+++ eclipse/curso-java-basico/src/com/cursojava/aula44/AnimalEstimacao.java
@@ -0,0 +1,11 @@
+package com.cursojava.aula44;
+
+//Interface: Classe abstrata sem argumentos e com métodos abstratos
+//Todos os métodos de interfaces são public abstract, então não há necessidade dessa informação ser escrita
+//Se houverem atributos, só podem ser public final
+public interface AnimalEstimacao {
+
+	void brincar();
+	void levarPassear();
+	
+}
