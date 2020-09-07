ff7c5aa531796e0fa1050c179b5ba3938473cc36
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-09-07 10:57:12 -0300

    Aula 42 - Palavra final

---
+++ eclipse/curso-java-basico/src/com/cursojava/aula42/Constantes.java
@@ -0,0 +1,9 @@
+package com.cursojava.aula42;
+
+public final class Constantes {
+
+	//Palavra final torna argumento como constante
+	public static final String URL_BLOG = "http://blog.com";
+	public static final String URL_CURSO_COMPLETO = "http://blog.curso.com";
+	
+}
